b6c661b83c35c3fa1ae7d7ee5b4cd325b3959a60
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2026-04-18 00:09:34 -0600

    change order of parameter sweep

--- rw_ratio_bench.sh
+++ rw_ratio_bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for isa in X86 ARM; do
-  for lsqsize in 16 32 64 128 256; do
-    for write_ratio in 0 25 50 75 100; do
+for write_ratio in 50 25 75 0 100; do
+  for isa in ARM X86; do
+    for lsqsize in 8 16 32 64 128; do
       echo "Running with ISA=$isa, SQ/LQ size=$lsqsize, write-ratio=$write_ratio"
       ./gem5/build/ALL/gem5.opt rw_ratio_bench.py \
         --binary-path rw_ratio_bench-${isa} \
