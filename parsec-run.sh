0b95c9dfde0ccce404fcf23da525d224d5d30958
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2026-04-17 23:03:12 -0600

    support ARM in parsec-run

--- parsec-run.sh
+++ parsec-run.sh
@@ -10,16 +10,27 @@ declare -A exp_simsize=(
 
 for NAME in "${!exp_simsize[@]}"; do
     simsize=${exp_simsize[$NAME]}
+
+    export NTHREADS=1
+    source $PARSEC_DIR/config/packages/parsec.$NAME.pkgconf
+    exppkgdir="$PARSEC_DIR/pkgs/${pkg_group}/$NAME"
+    source $exppkgdir/parsec/$simsize.runconf
+
     echo "Extracting input_$simsize.tar for $NAME..."
-    TAR_PATH="$PARSEC_DIR/parsec-3.0/pkgs/apps/$NAME/inputs/input_$simsize.tar"
+    TAR_PATH="$exppkgdir/inputs/input_$simsize.tar"
 
     if [ -f "$TAR_PATH" ]; then
         tar -xf "$TAR_PATH"
     fi
 
-    export NTHREADS=1
-    source $PARSEC_DIR/config/packages/parsec.$NAME.pkgconf
-    source $PARSEC_DIR/pkgs/${pkg_group}/$NAME/parsec/$simsize.runconf
     echo "Running $NAME with $simsize configuration..."
-    ./gem5/build/ALL/gem5.opt se_binary_workload.py --binary-path $PARSEC_DIR/pkgs/${pkg_group}/$NAME/inst/amd64-linux.gcc-hooks/bin/$NAME --binary-args "${run_args}"
+    arch=$(uname -m)
+    if [ "$arch" == "x86_64" ]; then
+        ./gem5/build/ALL/gem5.opt se_binary_workload.py --binary-path $exppkgdir/inst/amd64-linux.gcc-hooks/bin/$NAME --binary-args "${run_args}"
+    elif [ "$arch" == "aarch64" ]; then
+        ./gem5/build/ALL/gem5.opt se_binary_workload.py --isa ARM --binary-path $exppkgdir/inst/aarch64-linux.gcc-hooks/bin/$NAME --binary-args "${run_args}"
+    else
+        echo "Unsupported architecture: $arch"
+        exit 1
+    fi
 done
