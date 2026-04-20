5b55713e6cb7f111add8a8a512d36f7145f09c1c
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2026-04-20 03:24:33 -0600

    add splash2x in parsec build

--- parsec-build.sh
+++ parsec-build.sh
@@ -20,7 +20,7 @@ parsecmgmt -a build -p libtool
 parsecmgmt -a build -p hooks
 parsecmgmt -a build -p mesa
 
-for NAME in blackscholes bodytrack canneal facesim ferret fluidanimate freqmine streamcluster swaptions vips; do
+for NAME in blackscholes bodytrack canneal facesim ferret fluidanimate freqmine streamcluster swaptions vips splash2x.barnes splash2x.cholesky splash2x.fft splash2x.fmm splash2x.lu_cb splash2x.lu_ncb splash2x.ocean_cp splash2x.ocean_ncp splash2x.radiosity splash2x.radix splash2x.volrend splash2x.water_nsquared splash2x.water_spatial; do
     parsecmgmt -a build -p $NAME -c gcc-hooks
 done
 
