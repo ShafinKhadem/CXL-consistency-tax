b567ca5a89819f39409d53c865f69c7c26c02533
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2026-04-20 15:11:03 -0600

    add results for rw_ratio_bench with precise exceptions

--- rw_ratio_bench.sh
+++ rw_ratio_bench.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-for write_ratio in 50 25 75 0 100; do
+for write_ratio in 50 0 100 25 75; do
   for isa in ARM X86; do
-    for sqsize in 8 16 32 64 128 1; do
+    for sqsize in 8 16 32 64 128; do
       lqsize=$sqsize
-      if [ "$sqsize" -eq 1 ]; then
-        lqsize=128
-      fi
       echo "Running with ISA=$isa, SQ size=$sqsize, LQ size=$lqsize, write-ratio=$write_ratio"
       ./gem5/build/ALL/gem5.opt rw_ratio_bench.py \
         --binary-path rw_ratio_bench-${isa} \
